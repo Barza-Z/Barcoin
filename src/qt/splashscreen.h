26e1e9b3ee401b8a7dc273a65c9f5bf18c54440e
Barzanium <barzanium@outlook.com>
2021-12-01 08:18:17 +0000

    initial commit

---
+++ src/qt/splashscreen.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2011-2013 The Bitcoin developers
+ 
+ 
+
+#ifndef SPLASHSCREEN_H
+#define SPLASHSCREEN_H
+
+#include <QSplashScreen>
+
+/** class for the splashscreen with information of the running client
+ */
+class SplashScreen : public QSplashScreen
+{
+    Q_OBJECT
+
+public:
+    explicit SplashScreen(const QPixmap &pixmap = QPixmap(), Qt::WindowFlags f = 0);
+};
+
+#endif // SPLASHSCREEN_H
---
+++ src/qt/transactiondesc.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2011-2013 The Bitcoin developers
+ 
+ 
+
+#ifndef TRANSACTIONDESC_H
+#define TRANSACTIONDESC_H
+
+#include <QString>
+#include <QObject>
+
+class CWallet;
+class CWalletTx;
+
+/** Provide a human-readable extended HTML description of a transaction.
+ */
+class TransactionDesc: public QObject
+{
+    Q_OBJECT
+
+public:
+    static QString toHTML(CWallet *wallet, CWalletTx &wtx);
+
+private:
+    TransactionDesc() {}
+
+    static QString FormatTxStatus(const CWalletTx& wtx);
+};
+
+#endif // TRANSACTIONDESC_H
